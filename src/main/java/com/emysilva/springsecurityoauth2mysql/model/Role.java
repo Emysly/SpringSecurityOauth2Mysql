5bc1c7a8e5b1bf1eeae0eacabf5d0690a3a7c51b
emysilva <emysly12@gmail.com>
2020-12-10 12:15:27 +0100

    add role model

---
+++ src/main/java/com/emysilva/springsecurityoauth2mysql/model/Role.java
@@ -0,0 +1,39 @@
+package com.emysilva.springsecurityoauth2mysql.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "role")
+public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "role_id")
+    private int roleId;
+
+    @Column(name = "role")
+    private String role;
+
+    public Role() {
+    }
+
+    public Role(String role_user) {
+        this.role = role_user;
+    }
+
+    public int getRoleId() {
+        return roleId;
+    }
+
+    public void setRoleId(int roleId) {
+        this.roleId = roleId;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+}
