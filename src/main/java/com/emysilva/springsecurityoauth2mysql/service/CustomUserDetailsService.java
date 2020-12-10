1090e840ca6be26039be42d4bdacd9b30b4b6f8d
emysilva <emysly12@gmail.com>
2020-12-10 12:16:59 +0100

    add custom user details service

---
+++ src/main/java/com/emysilva/springsecurityoauth2mysql/service/CustomUserDetailsService.java
@@ -0,0 +1,31 @@
+package com.emysilva.springsecurityoauth2mysql.service;
+
+import com.emysilva.springsecurityoauth2mysql.model.*;
+import com.emysilva.springsecurityoauth2mysql.repository.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.*;
+
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+    @Autowired
+    private UsersRepository usersRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+        Optional<Users> usersOptional = usersRepository.findByName(username);
+
+        usersOptional
+                .orElseThrow(() -> new UsernameNotFoundException("Username not found!"));
+        return usersOptional
+                .map(CustomUserDetails::new)
+                .get();
+    }
+}
